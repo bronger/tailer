ffb97c0e9909be9c0b3636e8126b94f91832be8b
Torsten Bronger <bronger@physik.rwth-aachen.de>
2025-06-16 10:01:25 +0200

    Add logging

--- tailer.go
+++ tailer.go
@@ -3,21 +3,29 @@ package main
 import (
 	"errors"
 	"io"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
 	tbr_errors "gitlab.com/bronger/tools/errors"
+	tbr_logging "gitlab.com/bronger/tools/logging"
 	"go4.org/must"
 )
 
-func tail(f *os.File) {
-	_, err := io.Copy(os.Stdout, f)
+func init() {
+	tbr_logging.Init(os.Stderr, slog.LevelDebug)
+}
+
+func tail(logger tbr_logging.Logger, f *os.File) {
+	written, err := io.Copy(os.Stdout, f)
 	tbr_errors.ExitOnExpectedError(err, "Could not tail file", 9)
+	logger.Debug("Synchronised", "bytes", written)
 }
 
 func main() {
+	logger := slog.Default()
 	filePath, err := filepath.Abs(os.Args[1])
 	tbr_errors.ExitOnExpectedError(err, "File not found", 3, "path", filePath)
 	tailWatcher, err := fsnotify.NewWatcher()
@@ -33,23 +41,23 @@ func main() {
 	tbr_errors.ExitOnExpectedError(err, "Could not open original file", 8, "path", filePath)
 	defer must.Close(f)
 	if _, err = os.Stat(stopFilePath); err == nil {
-		tail(f)
+		tail(logger, f)
 		return
 	} else if !errors.Is(err, os.ErrNotExist) {
 		tbr_errors.ExitWithExpectedError("Could not access stop file", 10, "error", err, "path", stopFilePath)
 	}
-	tail(f)
+	tail(logger, f)
 	var latestSync time.Time
 	for {
 		select {
 		case event := <-tailWatcher.Events:
 			if true || time.Since(latestSync) > time.Second && event.Op&fsnotify.Write == fsnotify.Write {
-				tail(f)
+				tail(logger, f)
 				latestSync = time.Now()
 			}
 		case event := <-stopWatcher.Events:
 			if event.Op&fsnotify.Create == fsnotify.Create && event.Name == stopFilePath {
-				tail(f)
+				tail(logger, f)
 				return
 			}
 		case err := <-tailWatcher.Errors:
